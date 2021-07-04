525d939a0e02f09f56201ba8bc0b75a4a329f735
Jagdish <jagdishofficial77@gmail.com>
2021-07-04 18:20:03 +0530

    add dynamic port

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const http = require("http")
 
 const server = http.createServer((req, res) => {})
 
- server.listen(4000, () => {
+ server.listen(process.env.PORT || 4000, () => {
     console.log("Listening on port 4000...")
 }) 
 
